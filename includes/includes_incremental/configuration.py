59edc98f3c1d7283286f891923706bc4a2264095
manishsabbani2606 <sabbanimanish2606@gmail.com>
2025-05-14 20:55:24 -0500

    Add files via upload

---
+++ includes/includes_incremental/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "abfss://formula1@f1manishadls.dfs.core.windows.net/raw"
+processed_folder_path = "abfss://formula1@f1manishadls.dfs.core.windows.net/processed"
+presentation_folder_path = "abfss://formula1@f1manishadls.dfs.core.windows.net/presentation" 
+
+# COMMAND ----------
+
